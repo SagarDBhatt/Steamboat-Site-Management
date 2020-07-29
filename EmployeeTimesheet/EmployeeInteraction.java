c6b27076d49158553f93dab524ccd27f903b8384
Sbhatt <Sbhatt@revolutionsystems.net>
2020-07-29 06:48:20 -1000

    Update EmployeeWorkHours.java
    Update validations to insert correct data into SQL. Catch exceptions for frequently wrong data issues.

--- EmployeeTimesheet/EmployeeInteraction.java
+++ EmployeeTimesheet/EmployeeInteraction.java
@@ -10,23 +10,27 @@ import com.toedter.calendar.JDateChooser;
 import SteamboatSprings.SiteManagementAPI.ConnectionToSQL;
 
 public class EmployeeInteraction {
-	
+
 	public static void insertEmployeeData(String empName, Double workHours, JDateChooser dateChooser) {
 		java.sql.Date workDate = new java.sql.Date(dateChooser.getDate().getTime());
-		
-		String qryInsertToEmployeeTable = "  Insert into Employee(EmpName,HoursWorked,BreakTime,Date)\r\n" + 
-				"  Values('"+empName+"',"+workHours+",1,'"+workDate+"')";
-		
+
+		String qryInsertToEmployeeTable = "  Insert into Employee(EmpName,HoursWorked,BreakTime,Date)\r\n"
+				+ "  Values('" + empName + "'," + workHours + ",1,'" + workDate + "')";
+
 		try {
 			ConnectionToSQL.getaStatement().execute(qryInsertToEmployeeTable);
 			JOptionPane.showMessageDialog(null, "Data updated to Employee Table!", "Success",
 					JOptionPane.INFORMATION_MESSAGE);
 		} catch (SQLException e) {
-			e.printStackTrace();JOptionPane.showMessageDialog(null, "Unable to update data into Employee Table!", "Error!!",
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Unable to update data into Employee Table!", "Error!!",
 					JOptionPane.ERROR_MESSAGE);
 		}
-		
-		
-	}
+		catch (NumberFormatException e) {
+			e.printStackTrace();JOptionPane.showMessageDialog(null, "Please insert valid value!", "Error!!",
+					JOptionPane.ERROR_MESSAGE);
+		}
+
+	}//End of insertEmployeeData()
 
 }
