7c3fc2916a70dadd8b073d8c36a4e09134128717
Sbhatt <Sbhatt@revolutionsystems.net>
2020-07-29 05:59:59 -1000

    Create insertTOEmployee table method;Create ConnectionTOSQL.java
    Method to insert the data to Employee table.

--- ConnectionToSQL.java
+++ ConnectionToSQL.java
@@ -9,12 +9,38 @@ import javax.swing.JOptionPane;
 
 public class ConnectionToSQL {
 
-	static String JDBC_Driver = "com.mysql.cj.jdbc.Driver";
-	static String dbURL = "jdbc:sqlserver://FLEXBIZHTAPP\\SQLEXPRESS:1433;databaseName=Steamboat;integratedSecurity=true";
-	static String userName = "CTLIO/Sbhatt";
-	static Connection aConnection = null;
-	static Statement aStatement = null;
+	private static String JDBC_Driver = "com.mysql.cj.jdbc.Driver";
+	private static String dbURL = "jdbc:sqlserver://FLEXBIZHTAPP\\SQLEXPRESS:1433;databaseName=Steamboat;integratedSecurity=true";
+	private static String userName = "CTLIO/Sbhatt";
+	private static Connection aConnection = null;
+	private static Statement aStatement = null;
 	
+	
+	
+	public static Connection getaConnection() {
+		return aConnection;
+	}
+
+
+
+	public static void setaConnection(Connection aConnection) {
+		ConnectionToSQL.aConnection = aConnection;
+	}
+
+
+
+	public static Statement getaStatement() {
+		return aStatement;
+	}
+
+
+
+	public static void setaStatement(Statement aStatement) {
+		ConnectionToSQL.aStatement = aStatement;
+	}
+
+
+
 	public static void ConnectionSql() throws ClassNotFoundException, SQLException {
 
 		try {
--- EmployeeTimesheet/EmployeeInteraction.java
+++ EmployeeTimesheet/EmployeeInteraction.java
@@ -1,6 +1,9 @@
 package SteamboatSprings.SiteManagementAPI.EmployeeTimesheet;
 
 import java.sql.Date;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
 
 import com.toedter.calendar.JDateChooser;
 
@@ -10,6 +13,18 @@ public class EmployeeInteraction {
 	
 	public static void insertEmployeeData(String empName, Double workHours, JDateChooser dateChooser) {
 		java.sql.Date workDate = new java.sql.Date(dateChooser.getDate().getTime());
+		
+		String qryInsertToEmployeeTable = "  Insert into Employee(EmpName,HoursWorked,BreakTime,Date)\r\n" + 
+				"  Values('"+empName+"',"+workHours+",1,'"+workDate+"')";
+		
+		try {
+			ConnectionToSQL.getaStatement().execute(qryInsertToEmployeeTable);
+			JOptionPane.showMessageDialog(null, "Data updated to Employee Table!", "Success",
+					JOptionPane.INFORMATION_MESSAGE);
+		} catch (SQLException e) {
+			e.printStackTrace();JOptionPane.showMessageDialog(null, "Unable to update data into Employee Table!", "Error!!",
+					JOptionPane.ERROR_MESSAGE);
+		}
 		
 		
 	}
